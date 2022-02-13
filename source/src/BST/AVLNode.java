e50fc492a184206232fa2220f92caa4fa401d29f
수빙 <66006225+realhsb@users.noreply.github.com>
2022-02-14 00:52:26 +0900

    AVL node

---
+++ source/src/BST/AVLNode.java
@@ -0,0 +1,25 @@
+package BST;
+
+public class AVLNode {
+	public Comparable item;
+	public AVLNode left, right;
+	public int height;
+	
+	public AVLNode(Comparable newItem) {
+		item = newItem;
+		left = right = AVLTree.NIL;
+		height = 1;
+	}
+	
+	public AVLNode(Comparable newItem, AVLNode leftChild, AVLNode rightChild) {
+		item = newItem;
+		left = leftChild; right = rightChild;
+		height = 1;
+	}
+	
+	public AVLNode(Comparable newItem, AVLNode leftChild, AVLNode rightChild, int h) {
+		item = newItem;
+		left = leftChild; right = rightChild;
+		height = h;
+	}
+}
